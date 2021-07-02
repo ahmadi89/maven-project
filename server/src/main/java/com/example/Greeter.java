3afbf0e564c5b0319c728195fdf3b22b4986986d
ahmadi89 <80821145+ahmadi89@users.noreply.github.com>
2021-07-02 15:39:54 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,17 +1,27 @@
+/**
+ *An abstract class that represents an algorithm.
+ *
+ * @author Polo
+ *
+ * @version 1.0
+*/
 package com.example;
 
-
+/**
+ * this is a class.
+ */
 public class Greeter {
-
-  
+/**
+ * this is a constructor.
+ */
   public Greeter() {
 
   }
-
-  //public final String greet(final String someone) {
-    //return String.format("Hello babacar, %s!", someone);
-  //}
-  public String greet(final String someone) {
+/**
+ * @param someone name of person
+ * @return String
+ */
+  public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
 }
